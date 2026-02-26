dfcc6c6d169723f0f1366f4c47a31ae920d9298f
Marely <marelyjacome782@gmail.com>
2026-02-26 01:37:09 -0600

    agrego de Persna-auto

---
+++ Persona-auto/Auto.java
@@ -0,0 +1,55 @@
+
+import java.util.*;
+
+
+/**
+ * Class Auto
+ */
+public class Auto {
+
+  //
+  // Fields
+  //
+
+  private String marca;
+  
+  //
+  // Constructors
+  //
+  public Auto (String marca) {
+	this.marca = marca;
+ };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of marca
+   * @param newVar the new value of marca
+   */
+  public void setMarca (String marca) {
+    this.marca = marca;
+  }
+
+  /**
+   * Get the value of marca
+   * @return the value of marca
+   */
+  public String getMarca () {
+    return marca;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+
+}
---
+++ Persona-auto/Marca.java
@@ -0,0 +1,42 @@
+
+
+/**
+ * Class Marca
+ */
+public class Marca {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public Marca () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public static void main(String[] args)
+  {
+	Auto auto = new Auto("Toyota");
+	Persona persona = new Persona("Marely", auto);
+
+	persona.mostrarAuto();
+  }
+
+
+}
